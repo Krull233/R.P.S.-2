ba544262294d3b1a6e25df6b5918f7a534ac3773
Megan Holland-Robinson <1995megan@gmail.com>
2022-04-19 15:10:47 -0400

    Create roundOne function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
-const weapons = ["Rock", "Paper", "Scissors"];
+const weapons = ["rock", "paper", "scissors"];
+
+function roundOne(playerSelection, computerSelection) {
+  const player = prompt("Rock,Paper, or Scissors?".toLowerCase());
+  const computer = weapons[Math.floor(Math.random() * weapons.length)];
+
+  if ((player == "rock", computer == "scissors")) {
+    return "Player Wins!";
+  } else if ((player == "paper", computer == "rock")) {
+    return "Player wins!";
+  } else if ((player == "scissors", computer == "paper")) {
+    return "Player wins!";
+  } else {
+    return "Computer wins!";
+  }
+}
+
+console.log(roundOne());
 
 function computerPlay() {
   return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
-console.log(computerPlay());
+//console.log(computerPlay());

12026807732104ee995aa290c40d4f5489f89002
Megan Holland-Robinson <1995megan@gmail.com>
2022-04-25 10:08:03 -0400

    Complete Game

--- javascript.js
+++ javascript.js
@@ -1,24 +1,83 @@
 const weapons = ["rock", "paper", "scissors"];
 
 function roundOne(playerSelection, computerSelection) {
-  const player = prompt("Rock,Paper, or Scissors?".toLowerCase());
+  const player = prompt("Rock, Paper, or Scissors?".toLowerCase());
   const computer = weapons[Math.floor(Math.random() * weapons.length)];
-
-  if ((player == "rock", computer == "scissors")) {
-    return "Player Wins!";
-  } else if ((player == "paper", computer == "rock")) {
+  console.log(player, computer);
+  if (player == "rock" && computer == "scissors") {
     return "Player wins!";
-  } else if ((player == "scissors", computer == "paper")) {
+  } else if (player == "paper" && computer == "rock") {
     return "Player wins!";
+  } else if (player == "scissors" && computer == "paper") {
+    return "Player wins!";
+  } else if (player == computer) {
+    return "It's a tie!";
   } else {
     return "Computer wins!";
   }
 }
 
-console.log(roundOne());
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let tieScore = 0;
+  result = roundOne();
 
-function computerPlay() {
-  return weapons[Math.floor(Math.random() * weapons.length)];
+  if (result == "Player wins!") {
+    playerScore++;
+  } else if (result == "Computer wins!") {
+    computerScore++;
+  } else {
+    tieScore++;
+  }
+
+  result = roundOne();
+
+  if (result == "Player wins!") {
+    playerScore++;
+  } else if (result == "Computer wins!") {
+    computerScore++;
+  } else {
+    tieScore++;
+  }
+
+  result = roundOne();
+
+  if (result == "Player wins!") {
+    playerScore++;
+  } else if (result == "Computer wins!") {
+    computerScore++;
+  } else {
+    tieScore++;
+  }
+
+  result = roundOne();
+
+  if (result == "Player wins!") {
+    playerScore++;
+  } else if (result == "Computer wins!") {
+    computerScore++;
+  } else {
+    tieScore++;
+  }
+
+  result = roundOne();
+
+  if (result == "Player wins!") {
+    playerScore++;
+  } else if (result == "Computer wins!") {
+    computerScore++;
+  } else {
+    tieScore++;
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Player wins the game!");
+  } else if (computerScore > playerScore) {
+    console.log("Computer wins the game!");
+  } else {
+    console.log("It's a tie!");
+  }
 }
 
-//console.log(computerPlay());
+game();
